695ad42459bcd61a3e7dfef401f40f948e5dec7e
tienhuynh-tn <tien.huynhlt.tn@gmail.com>
2025-01-28 23:32:34 +0700

    LL Constructor exercise and remove .idea folder

---
+++ udemy-java-dsa-and-leetcode-exercises/linked-list/ll-constructor/src/main/java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(4);
+
+        myLinkedList.printAll();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Head: 4
+            Tail: 4
+            Length: 1
+
+            Linked List:
+            4
+        */
+    }
+}
